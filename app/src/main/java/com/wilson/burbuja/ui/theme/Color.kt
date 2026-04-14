0671beb5b2e04a8902dc2f67c1a2134f240c1369
win10 <wilsoncabrera307@gmail.com>
2026-04-14 00:41:08 -0300

    merge: integrando archivos de github

---
+++ app/src/main/java/com/wilson/burbuja/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.wilson.burbuja.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Paleta Tecno-Minimalista de Burbuja
+val BackgroundDark = Color(0xFF1F2A37)
+val AccentCyan = Color(0xFF7BCBFF)
+val AccentViolet = Color(0xFF6A5CFF)
+
+val ColorBarra = Color(0xFF6E88A6)
+
+// Colores extra para textos y contrastes
+val TextWhite = Color(0xFFEEEEEE)
+val TextGray = Color(0xFFA0AAB2)
+val SurfaceDark = Color(0xFF2A3644)
